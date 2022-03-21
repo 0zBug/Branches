f09ed1dafd68f97b920210d0aa1881830f3afcee
Bug <77031032+0zBug@users.noreply.github.com>
2022-03-21 11:34:57 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 
 local function ParseChild(Tree, Level, Connector)
     local str = "\n"
+    local Level = Level or 0
     for i,v in next, Tree do
         str = str .. "│    "
         if Connector then
@@ -16,6 +17,7 @@ local function ParseChild(Tree, Level, Connector)
         end
 
         str = str .. v.Name
+        if not v.Children then v.Children = {} end
         if v.Children then
             str = str .. ParseChild(v.Children, Level + 1, i ~= #Tree)
         end
@@ -26,7 +28,7 @@ end
 
 local Branches = {}
 function Branches.new(Tree)
-    return string.format("\n├── %s", Tree.Name) .. ParseChild(Tree.Children, level or 0)
+    return string.format("\n├── %s", Tree.Name) .. ParseChild(Tree.Children)
 end
 
 return Branches
